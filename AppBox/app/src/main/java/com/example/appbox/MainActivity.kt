cc13c0059bf946baace48620d598eea933f21b5e
Artur Mello <artursbm@github.com>
2020-04-05 13:35:27 -0300

    started using RecyclerView instead of ListView to make a more efficient todo list

--- AppBox/app/src/main/java/com/example/appbox/MainActivity.kt
+++ AppBox/app/src/main/java/com/example/appbox/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.example.appbox
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        // this will add the layout manager that makes recycler view works,
+        // and will set the todoList to be managed by the layout manager
+        linearLayoutManager = LinearLayoutManager(this)
+        todoList.layoutManager = linearLayoutManager
 
         var itemList = arrayListOf<String>()
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, itemList)
+//        var adapter = ListAdapter<String>(this, android.R.layout.simple_selectable_list_item, itemList)
 
         addNote.setOnClickListener { view ->
-            itemList.add(todoItem.text.toString())
-            todoList.adapter = adapter
-            adapter.notifyDataSetChanged()
-            todoItem.text = null
+            itemList.add(newTodoItem.text.toString())
+//            todoList.adapter = adapter
+//            adapter.notifyDataSetChanged()
+            newTodoItem.text = null
             Snackbar.make(view, R.string.added_idea, Snackbar.LENGTH_SHORT).show()
 
         }
